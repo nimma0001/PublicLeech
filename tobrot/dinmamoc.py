489c285d913046e9439e957b253d070821f02958
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2020-10-03 11:49:20 +0530

    Update dinmamoc.py

--- tobrot/dinmamoc.py
+++ tobrot/dinmamoc.py
@@ -46,6 +46,38 @@ class Commandi:
         "COMMANDI_EVAL",
         "eval"
     )
+    RENAfrom tobrot.get_cfg import get_config
+
+
+class Commandi:
+    LEECH = get_config(
+        "COMMANDI_LEECH",
+        "leech"
+    )
+    PURGE = get_config(
+        "COMMANDI_PURGE",
+        "purge"
+    )
+    YTDL = get_config(
+        "COMMANDI_YTDL",
+        "ytdl"
+    )
+    STATUS = get_config(
+        "COMMANDI_STATUS",
+        "status"
+    )
+    CANCEL = get_config(
+        "COMMANDI_CANCEL",
+        "cancel"
+    )
+    EXEC = get_config(
+        "COMMANDI_EXEC",
+        "exec"
+    )
+    EVAL = get_config(
+        "COMMANDI_EVAL",
+        "eval"
+    )
     RENAME = get_config(
         "COMMANDI_RENAME",
         "rename"
@@ -57,6 +89,85 @@ class Commandi:
     HELP = get_config(
         "COMMANDI_HELP",
         "help"
+    )
+    SAVETHUMBNAIL = get_config(
+        "COMMANDI_SAVETHUMBNAIL",
+        "savethumbnail"
+    )
+    CLEARTHUMBNAIL = get_config(
+        "COMMANDI_CLEARTHUMBNAIL",
+        "clearthumbnail"
+    )
+    GET_RCLONE_CONF_URI = get_configME = get_config(
+        "COMMANDI_RENAME",
+        "rename"
+    )
+    UPLOAD = get_config(
+        "COMMANDI_UPLOAD",
+        "upload"
+    )
+    HELP = get_config(
+        "COMMANDI_HELP",
+        "help"
+    )from tobrot.get_cfg import get_config
+
+
+class Commandi:
+    LEECH = get_config(
+        "COMMANDI_LEECH",
+        "leech@speedlestbot"
+    )
+    PURGE = get_config(
+        "COMMANDI_PURGE",
+        "purge@speedlestbot"
+    )
+    YTDL = get_config(
+        "COMMANDI_YTDL",
+        "ytdl@speedlestbot"
+    )
+    STATUS = get_config(
+        "COMMANDI_STATUS",
+        "status@speedlestbot"
+    )
+    CANCEL = get_config(
+        "COMMANDI_CANCEL",
+        "cancel@speedlestbot"
+    )
+    EXEC = get_config(
+        "COMMANDI_EXEC",
+        "exec@speedlestbot"
+    )
+    EVAL = get_config(
+        "COMMANDI_EVAL",
+        "eval@speedlestbot"
+    )
+    RENAME = get_config(
+        "COMMANDI_RENAME",
+        "rename@speedlestbot"
+    )
+    UPLOAD = get_config(
+        "COMMANDI_UPLOAD",
+        "upload@speedlestbot"
+    )
+    HELP = get_config(
+        "COMMANDI_HELP",
+        "help"
+    )
+    SAVETHUMBNAIL = get_config(
+        "COMMANDI_SAVETHUMBNAIL",
+        "savethumbnail@speedlestbot"
+    )
+    CLEARTHUMBNAIL = get_config(
+        "COMMANDI_CLEARTHUMBNAIL",
+        "clearthumbnail@speedlestbot"
+    )
+    GET_RCLONE_CONF_URI = get_config(
+        "COMMANDI_GET_RCLONE_CONF_URI",
+        "getrcloneconfuri"
+    )
+    UPLOAD_LOG_FILE = get_config(
+        "COMMANDI_UPLOAD_LOG_FILE",
+        "log"
     )
     SAVETHUMBNAIL = get_config(
         "COMMANDI_SAVETHUMBNAIL",
